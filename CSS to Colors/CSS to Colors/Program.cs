1e3592f04b31df450b1926d4bf98c97cc95b41e4
sondow77 <sondow1@hotmail.com>
2017-01-20 01:56:41 -0300

    Two converters added
    Added the source code of the two converters, from Colors Telegram Theme file to CSS and CSS to Colors Telegram Theme file

---
+++ CSS to Colors/CSS to Colors/Program.cs
@@ -0,0 +1,64 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sondo
+ * Date: 18/1/2017
+ * Time: 4:48 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.IO;
+
+namespace CSS_to_Colors
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			if (args.Length == 2)
+	        {
+				string cssCode = File.ReadAllText(args[0]);
+				string cCode = cssCode.Replace(".","").Replace("\r\n{\r\n\t","").Replace("background-","").Replace("color","").Replace(" : ",": ").Replace(":hover","Over").Replace(":focus","Active").Replace(":disabled","Inactive").Replace(":active","Ripple").Replace("\r\n}","");
+				string[] stringSeparators = new string[] { "\r\n" };
+				string[] cssA = cCode.Split(stringSeparators,StringSplitOptions.None);
+				for(int i = 0; i < cssA.Length;i++)
+				{
+					//rgba\((\d{1,3},\s*){3}\d{1,3}\)
+					string rgba = Regex.Match(cssA[i],@"rgba\((\d{1,3},\s*){3}\d{1}.{0,1}\d+\)",RegexOptions.IgnoreCase).Value;
+					cssA[i] = Regex.Replace(cssA[i],@"rgba\((\d{1,3},\s*){3}\d{1}.{0,1}\d+\)",ConvertRgbaToHex(rgba),RegexOptions.IgnoreCase);
+				}
+				File.WriteAllLines(args[1],cssA);
+			}
+			else
+			{
+				System.Console.WriteLine("Please enter input and output arguments...");
+	            Console.ReadKey(true);
+			}
+		}
+		public static string ConvertRgbaToHex(string rgba)
+		{
+			string result = "";
+			if (Regex.IsMatch(rgba, @"rgba\((\d{1,3},\s*){3}\d{1}.{0,1}\d+\)",RegexOptions.IgnoreCase))
+			{
+				result = "#";
+				MatchCollection cols = Regex.Matches(rgba,@"\d+(\.{0,1})(\d+|)",RegexOptions.IgnoreCase);
+				decimal a = Math.Round(decimal.Parse(cols[3].Value.Insert(1,","),NumberStyles.AllowDecimalPoint) * 255);
+				result += DecToHex(cols[0].Value) + DecToHex(cols[1].Value) + DecToHex(cols[2].Value) + DecToHex(a.ToString());
+			}
+			return result;
+		}
+		public static string DecToHex(string num)
+		{
+			string hex = String.Format("{0:X}", int.Parse(num));
+			if(hex.Length == 1)
+			{
+				hex = "0" + hex;
+			}
+			return hex;
+		}
+	}
+}
---
+++ Colors to CSS/Colors to CSS/Program.cs
@@ -0,0 +1,71 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: sondo
+ * Date: 17/1/2017
+ * Time: 5:06 p. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace Colors_to_CSS
+{
+	class Program
+	{
+		public static string ConvertHexToRgba(string hex)
+		{
+			string result = "";
+			if (hex.StartsWith("#"))
+			{
+            	hex = hex.Substring(1);
+			}
+	        if (hex.Length == 8)
+	        {
+	        	double alpha = (double)int.Parse(hex.Substring(6, 2), System.Globalization.NumberStyles.HexNumber)/255;
+	        	result = "rgba(" + int.Parse(hex.Substring(0, 2), System.Globalization.NumberStyles.HexNumber) + ", " +
+	            int.Parse(hex.Substring(2, 2), System.Globalization.NumberStyles.HexNumber) + ", " +
+	        	int.Parse(hex.Substring(4, 2), System.Globalization.NumberStyles.HexNumber) + ", " +
+	        	alpha.ToString().Replace(",",".") + ")";
+	        }
+	        return result;
+		}
+		public static void Main(string[] args)
+		{
+			const string pat = @"[a-zA-Z0-9]+: #(?:[0-9a-fA-F]{2}){3,4};";
+			if (args.Length == 2)
+	        {
+				string colorCode = File.ReadAllText(args[0]);
+				Regex r = new Regex(pat, RegexOptions.IgnoreCase);
+				Match onlyDec = r.Match(colorCode);
+				
+				var al = new List<string>();
+				while(onlyDec.Success)
+				{
+					al.Add(onlyDec.Value);
+					onlyDec = onlyDec.NextMatch();
+				}
+				string[] css = al.ToArray();
+				for(int i = 0; i < css.Length; i++)
+				{
+					css[i] = "." + css[i].Replace(":"," :").Replace("Active :",": focus :").Replace("Inactive :",": disabled :").Replace("Ripple :",": active :").Replace("Over :",": hover :").Replace(" :","\r\n{\r\n\tbackground-color :").Replace(";",";\r\n}").Replace(": hover",":hover").Replace(": focus",":focus").Replace(": disabled",":disabled").Replace(": active",":active");
+					if(css[i].Contains("Fg"))
+					{
+						css[i] = css[i].Replace("background-color","color");
+					}
+					string hex = Regex.Match(css[i],@"#[0-9a-fA-F]{8}",RegexOptions.IgnoreCase).Value;
+					css[i] = Regex.Replace(css[i],@"#[0-9a-fA-F]{8}",ConvertHexToRgba(hex));
+				}
+				
+				File.WriteAllLines(args[1],css);
+			}
+			else
+			{
+				System.Console.WriteLine("Please enter input and output arguments...");
+	            Console.ReadKey(true);
+			}
+		}
+	}
+}
